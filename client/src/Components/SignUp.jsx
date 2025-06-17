f3b81160efbc505c36c0320e9bba894097e15c84
Shakshi Vishwakarma <vishwakarmasakshi80@gmail.com>
2025-06-18 02:14:13 +0530

    signup page done

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link,useNavigate} from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { userSignUp  } from '../api/index';
+
 const SignUp = () => {
+  const [formData, setFormData] = React.useState({
+    name: '', 
+    email: '',
+    password: ''
+  });
+  const navigate = useNavigate();
+
+  const handleChnage = (e) => {
+   const { name, value } = e.target;
+   setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  }
+  const handleSubmit =  async (e) => {
+    e.preventDefault();
+    const response =await userSignUp(formData)
+
+    if (response.data.success){
+      navigate('/signin');
+    }else{
+      Swal.fire({
+        icon: 'error',
+        title: 'Account Failed to  create',
+        text: response.data.message || "invalid credentials",
+        timer: 2500,
+        showConfirmButton: false,
+      });
+
+      setFormData({
+        name: '',
+        email: '',
+        password: ''
+      });
+    }
+  }
+
+
   return (
-    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+
+
+    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800" onSubmit={handleSubmit}>
       {/* Title */}
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">Sign Up</p>
@@ -12,21 +55,27 @@ const SignUp = () => {
       {/* Inputs */}
       <input
         type="text"
+        name="name"
         className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-2 focus:ring-black"
         placeholder="Name"
         required
+        onChange={handleChnage}
       />
       <input
         type="email"
+        name="email"
         className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-2 focus:ring-black"
         placeholder="Email"
         required
+        onChange={handleChnage}
       />
       <input
         type="password"
+        name="password"
         className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-2 focus:ring-black"
         placeholder="Password"
         required
+        onChange={handleChnage}
       />
 
       {/* Links */}
