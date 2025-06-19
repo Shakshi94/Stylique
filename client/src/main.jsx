f3c96cdceb86dc5f4d97538048b0b06af0fe7409
Shakshi Vishwakarma <98729339+Shakshi94@users.noreply.github.com>
2025-06-19 12:13:08 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
-import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
