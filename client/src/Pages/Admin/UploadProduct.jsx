007b13cd5b35527fe47e58402aaaf837d68a6952
Shakshi Vishwakarma <98729339+Shakshi94@users.noreply.github.com>
2025-06-19 23:33:10 +0530

    Update UploadProduct.jsx

--- client/src/Pages/Admin/UploadProduct.jsx
+++ client/src/Pages/Admin/UploadProduct.jsx
@@ -6,6 +6,7 @@ import { useOutletContext } from 'react-router-dom';
 import { addProduct } from '../../api/index';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import AdminNavbar from '../../Components/AdminNavbar';
 
 const  UploadProduct = () => {
 
@@ -83,28 +84,7 @@ const  UploadProduct = () => {
     return (
       <div className="flex flex-col flex-1 overflow-y-auto bg-gray-50 min-h-screen">
         {/* Header */}
-        <div className="flex flex-wrap items-center justify-between h-auto p-4 bg-white border-b border-gray-200">
-          <div className="flex items-center w-full md:w-auto mb-2 md:mb-0">
-            <button
-              className="text-gray-500 focus:outline-none md:hidden"
-              onClick={() => setMobileOpen(true)}
-            >
-              <MenuIcon className="h-6 w-6" />
-            </button>
-            <input
-              className="w-full sm:w-64 border rounded-md px-4 py-2 text-sm"
-              type="text"
-              placeholder="Search"
-            />
-          </div>
-          <div className="flex items-center">
-            <button className="flex items-center text-gray-500 hover:text-gray-700 focus:outline-none">
-              <NavigateBeforeIcon className="h-6 w-6" />
-              <NavigateNextIcon className="h-6 w-6" />
-            </button>
-          </div>
-        </div>
-
+        < AdminNavbar/>    
         {/* Form Section */}
         <div className="p-5">
           <div className="inline-flex items-center gap-2 mb-6">
