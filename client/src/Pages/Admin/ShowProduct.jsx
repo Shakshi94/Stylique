8e00ff2fe3cf5723126a65d0cd7db960a774829c
Shakshi Vishwakarma <98729339+Shakshi94@users.noreply.github.com>
2025-06-19 23:32:15 +0530

    Update ShowProduct.jsx

--- client/src/Pages/Admin/ShowProduct.jsx
+++ client/src/Pages/Admin/ShowProduct.jsx
@@ -7,6 +7,7 @@ import {showProducts,deleteProduct} from '../../api/index'
 import { useSelector } from 'react-redux';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import AdminNavbar from '../../Components/AdminNavbar';
 
 function ShowProduct() {
 
@@ -49,27 +50,7 @@ function ShowProduct() {
     return (
 <div className="flex flex-col flex-1 overflow-y-auto">
   {/* Header */}
-  <div className="flex flex-wrap items-center justify-between h-auto p-4 bg-white border-b border-gray-200">
-    <div className="flex items-center w-full md:w-auto mb-2 md:mb-0">
-      <button
-        className="text-gray-500 focus:outline-none md:hidden"
-        onClick={() => setMobileOpen(true)}
-      >
-        <MenuIcon className="h-6 w-6" />
-      </button>
-      <input
-        className="w-full sm:w-64 border rounded-md px-4 py-2 text-sm"
-        type="text"
-        placeholder="Search"
-      />
-    </div>
-    <div className="flex items-center">
-      <button className="flex items-center text-gray-500 hover:text-gray-700 focus:outline-none">
-        <NavigateBeforeIcon className="h-6 w-6" />
-        <NavigateNextIcon className="h-6 w-6" />
-      </button>
-    </div>
-  </div>
+    <AdminNavbar/>
 
   {/* Product List */}
   <div className="p-4 w-full overflow-x-auto">
